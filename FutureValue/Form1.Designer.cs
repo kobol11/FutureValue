f3758728275f551e7d9d284ebb75b8ef1a45b79f
Bolarinwa Komolafe <komolafe.bolarinwa@outlook.com>
2018-09-08 09:41:23 -0400

    Add monthly investment label

--- FutureValue/Form1.Designer.cs
+++ FutureValue/Form1.Designer.cs
@@ -28,20 +28,34 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.lbl_monthlyinvetsment = new System.Windows.Forms.Label();
             this.SuspendLayout();
+            // 
+            // lbl_monthlyinvetsment
+            // 
+            this.lbl_monthlyinvetsment.AutoSize = true;
+            this.lbl_monthlyinvetsment.Location = new System.Drawing.Point(30, 44);
+            this.lbl_monthlyinvetsment.Name = "lbl_monthlyinvetsment";
+            this.lbl_monthlyinvetsment.Size = new System.Drawing.Size(102, 13);
+            this.lbl_monthlyinvetsment.TabIndex = 0;
+            this.lbl_monthlyinvetsment.Text = "Monthly Investment:";
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.lbl_monthlyinvetsment);
             this.Name = "Form1";
             this.Text = "Future Value";
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.Label lbl_monthlyinvetsment;
     }
 }
 

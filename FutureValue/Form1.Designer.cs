fff5a8271d7298c21b7597bd76caaf80303f1e5c
Bolarinwa Komolafe <komolafe.bolarinwa@outlook.com>
2018-09-08 10:16:12 -0400

    Add calculate and exit butttons

--- FutureValue/Form1.Designer.cs
+++ FutureValue/Form1.Designer.cs
@@ -32,6 +32,12 @@
             this.lbl_yearlyinterestrate = new System.Windows.Forms.Label();
             this.lbl_NoOfYears = new System.Windows.Forms.Label();
             this.lbl_FutureValue = new System.Windows.Forms.Label();
+            this.txb_MonthlyInvestment = new System.Windows.Forms.TextBox();
+            this.txb_YearlyInterestRate = new System.Windows.Forms.TextBox();
+            this.txb_NoOfYears = new System.Windows.Forms.TextBox();
+            this.txb_FutureValue = new System.Windows.Forms.TextBox();
+            this.btn_Calculate = new System.Windows.Forms.Button();
+            this.btn_Exit = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // lbl_monthlyinvetsment
@@ -46,7 +52,7 @@
             // lbl_yearlyinterestrate
             // 
             this.lbl_yearlyinterestrate.AutoSize = true;
-            this.lbl_yearlyinterestrate.Location = new System.Drawing.Point(30, 77);
+            this.lbl_yearlyinterestrate.Location = new System.Drawing.Point(30, 80);
             this.lbl_yearlyinterestrate.Name = "lbl_yearlyinterestrate";
             this.lbl_yearlyinterestrate.Size = new System.Drawing.Size(103, 13);
             this.lbl_yearlyinterestrate.TabIndex = 1;
@@ -55,7 +61,7 @@
             // lbl_NoOfYears
             // 
             this.lbl_NoOfYears.AutoSize = true;
-            this.lbl_NoOfYears.Location = new System.Drawing.Point(30, 114);
+            this.lbl_NoOfYears.Location = new System.Drawing.Point(30, 116);
             this.lbl_NoOfYears.Name = "lbl_NoOfYears";
             this.lbl_NoOfYears.Size = new System.Drawing.Size(89, 13);
             this.lbl_NoOfYears.TabIndex = 2;
@@ -70,16 +76,73 @@
             this.lbl_FutureValue.TabIndex = 3;
             this.lbl_FutureValue.Text = "Future Value:";
             // 
+            // txb_MonthlyInvestment
+            // 
+            this.txb_MonthlyInvestment.Location = new System.Drawing.Point(186, 37);
+            this.txb_MonthlyInvestment.Name = "txb_MonthlyInvestment";
+            this.txb_MonthlyInvestment.Size = new System.Drawing.Size(100, 20);
+            this.txb_MonthlyInvestment.TabIndex = 4;
+            // 
+            // txb_YearlyInterestRate
+            // 
+            this.txb_YearlyInterestRate.Location = new System.Drawing.Point(186, 73);
+            this.txb_YearlyInterestRate.Name = "txb_YearlyInterestRate";
+            this.txb_YearlyInterestRate.Size = new System.Drawing.Size(100, 20);
+            this.txb_YearlyInterestRate.TabIndex = 5;
+            // 
+            // txb_NoOfYears
+            // 
+            this.txb_NoOfYears.Location = new System.Drawing.Point(186, 109);
+            this.txb_NoOfYears.Name = "txb_NoOfYears";
+            this.txb_NoOfYears.Size = new System.Drawing.Size(100, 20);
+            this.txb_NoOfYears.TabIndex = 6;
+            // 
+            // txb_FutureValue
+            // 
+            this.txb_FutureValue.Location = new System.Drawing.Point(186, 145);
+            this.txb_FutureValue.Name = "txb_FutureValue";
+            this.txb_FutureValue.ReadOnly = true;
+            this.txb_FutureValue.Size = new System.Drawing.Size(100, 20);
+            this.txb_FutureValue.TabIndex = 7;
+            // 
+            // btn_Calculate
+            // 
+            this.btn_Calculate.Location = new System.Drawing.Point(79, 207);
+            this.btn_Calculate.Name = "btn_Calculate";
+            this.btn_Calculate.Size = new System.Drawing.Size(75, 23);
+            this.btn_Calculate.TabIndex = 8;
+            this.btn_Calculate.Text = "Calculate";
+            this.btn_Calculate.UseVisualStyleBackColor = true;
+            // 
+            // btn_Exit
+            // 
+            this.btn_Exit.DialogResult = System.Windows.Forms.DialogResult.Cancel;
+            this.btn_Exit.Location = new System.Drawing.Point(211, 207);
+            this.btn_Exit.Name = "btn_Exit";
+            this.btn_Exit.Size = new System.Drawing.Size(75, 23);
+            this.btn_Exit.TabIndex = 9;
+            this.btn_Exit.Text = "Exit";
+            this.btn_Exit.UseVisualStyleBackColor = true;
+            // 
             // Form1
             // 
+            this.AcceptButton = this.btn_Calculate;
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.CancelButton = this.btn_Exit;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btn_Exit);
+            this.Controls.Add(this.btn_Calculate);
+            this.Controls.Add(this.txb_FutureValue);
+            this.Controls.Add(this.txb_NoOfYears);
+            this.Controls.Add(this.txb_YearlyInterestRate);
+            this.Controls.Add(this.txb_MonthlyInvestment);
             this.Controls.Add(this.lbl_FutureValue);
             this.Controls.Add(this.lbl_NoOfYears);
             this.Controls.Add(this.lbl_yearlyinterestrate);
             this.Controls.Add(this.lbl_monthlyinvetsment);
             this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Future Value";
             this.ResumeLayout(false);
             this.PerformLayout();
@@ -92,6 +155,12 @@
         private System.Windows.Forms.Label lbl_yearlyinterestrate;
         private System.Windows.Forms.Label lbl_NoOfYears;
         private System.Windows.Forms.Label lbl_FutureValue;
+        private System.Windows.Forms.TextBox txb_MonthlyInvestment;
+        private System.Windows.Forms.TextBox txb_YearlyInterestRate;
+        private System.Windows.Forms.TextBox txb_NoOfYears;
+        private System.Windows.Forms.TextBox txb_FutureValue;
+        private System.Windows.Forms.Button btn_Calculate;
+        private System.Windows.Forms.Button btn_Exit;
     }
 }
 
